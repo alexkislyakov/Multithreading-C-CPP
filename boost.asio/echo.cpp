65024f9d5cf7f27f688a610f8a1e39c41b5dfa27
alexkislyakov <alex_kislyakov96@mail.ru>
2017-12-24 17:50:13 +0300

    stepic

--- boost.asio/echo.cpp
+++ boost.asio/echo.cpp
@@ -18,17 +18,16 @@ class session {
     
     void start() {
 	socket_.async_read_some(boost::asio::buffer(data_, max_length),
-                            boost::bind(&session::handle_read, this,
-                                        boost::asio::placeholders::error,
-                                        boost::asio::placeholders::bytes_transferred));
+                                boost::bind(&session::handle_read, this,
+                                            boost::asio::placeholders::error,
+                                            boost::asio::placeholders::bytes_transferred));
     }
     
     void handle_read(const boost::system::error_code &error, size_t bytes_transferred) {
 	if (!error) {
 	    boost::asio::async_write(socket_, boost::asio::buffer(data_, bytes_transferred),
-				                 boost::bind(&session::handle_write, this,
-						                     boost::asio::placeholders::error));
-
+				     boost::bind(&session::handle_write, this,
+						 boost::asio::placeholders::error));
 	} else {
 	    delete this;
 	}
@@ -37,9 +36,9 @@ class session {
     void handle_write(const boost::system::error_code &error) {
 	if (!error) {
 	    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-				                boost::bind(&session::handle_read, this,
-						                    boost::asio::placeholders::error,
-						                    boost::asio::placeholders::bytes_transferred));
+				    boost::bind(&session::handle_read, this,
+						boost::asio::placeholders::error,
+						boost::asio::placeholders::bytes_transferred));
 	} else {
 	    delete this;
 	}
@@ -52,12 +51,12 @@ class server {
     tcp::acceptor acceptor_;
  public:
     server(boost::asio::io_service &io_service, short port) : io_service_(io_service),
-							                                  acceptor_(io_service,
-									                                    tcp::endpoint(tcp::v4(), port)) {
+							      acceptor_(io_service,
+									tcp::endpoint(tcp::v4(), port)) {
 	session *new_session = new session(io_service_);
 	acceptor_.async_accept(new_session->socket(), boost::bind(&server::handle_accept,
-								                              this, new_session,
-                                                              boost::asio::placeholders::error));
+								  this, new_session,
+                                                                  boost::asio::placeholders::error));
     }
     
     void handle_accept(session *new_session, const boost::system::error_code &error) {
@@ -65,8 +64,8 @@ class server {
 	    new_session->start();
 	    new_session = new session(io_service_);
 	    acceptor_.async_accept(new_session->socket(), boost::bind(&server::handle_accept,
-								                                  this, new_session,
-                                                                  boost::asio::placeholders::error));
+								      this, new_session,
+                                                                      boost::asio::placeholders::error));
 	} else {
 	    delete new_session;
 	}
